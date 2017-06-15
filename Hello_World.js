a978b4324ef62da5633152ed45216d0f4e83c5f5
unknown <clairecardino15@gmail.com>
2017-06-15 12:21:23 +0800

    1st commit

---
+++ Hello_World.js
@@ -0,0 +1,6 @@
+module.exports = function(input) {
+  return input.toUpperCase()
+}
+
+//to.UpperCase()method convert a string to uppercase letters
+// module.exports is a special object which is included in every JS file in the Node.js application by default.
